d985be30d3bb3da8015f57e39e4f71150b408ded
vivek7088 <vivekpal7302@gmial.com>
2024-02-18 18:17:07 +0530

    Thise is second rectangle hollowrect

---
+++ Hollowrect.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Hollowrect {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.close();
+        // outer lood
+        for (int i = 1; i <= n; i++) {
+            // inner loop
+            for (int j = 1; j <= m; j++) {
+                // cell -> (i,j)
+                if ( i == 1 || j == 1 || i == n || j == m) {
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }       
+                
+            }
+            System.out.println();
+        }
+    }
+
+}
+
